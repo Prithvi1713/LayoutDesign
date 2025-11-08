d33d98b260130cd9b168a6d51301f6ed950ae3a9
Prithvi1713 <plavate95@gmail.com>
2025-11-08 21:36:00 +0530

    Add project files.

---
+++ LayoutDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LayoutDesign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
